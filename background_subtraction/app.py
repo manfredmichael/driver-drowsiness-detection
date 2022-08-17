b6afc8e1dc72cdc71712dde89ebe303cd5b27bc9
manfredmichael <koskosanrider@gmail.com>
2022-08-17 15:36:03 +0700

    Update requirements.txt

--- background_subtraction/app.py
+++ background_subtraction/app.py
@@ -23,13 +23,9 @@ def serve_pil_image(pil_img):
 @app.route("/predict", methods=['POST'])
 def make_prediction():
     if request.method == 'POST':
-        # file = request.files['file']
         webcam = request.files['webcam'].read()
         background = request.files['background'].read()
         virtual_background = request.files['virtual_background'].read()
-        # webcam = data['webcam']
-        # background = data['background']
-        # virtual_background = data['virtual_background']
 
         webcam = np.array(Image.open(BytesIO(decodebytes(webcam))))
         background = np.array(Image.open(BytesIO(decodebytes(background))))
@@ -37,28 +33,7 @@ def make_prediction():
 
         result = apply_virtual_background(webcam, background, virtual_background)
         result = Image.fromarray(np.uint8(result))
-        # annotations = json.load(request.files['data'])['annotations']
-        # image = Image.open(BytesIO(file.read()))
-        # result = detect_drowsiness(image)
         return serve_pil_image(result)
-
-@app.route('/get_images',methods=['GET'])
-def get_images():
-
-    ##reuslt  contains list of path images
-    encoded_imges = []
-    for image_path in result:
-        encoded_imges.append(get_response_image(image_path))
-    return jsonify({'result': encoded_imges})
-
-# @app.route("/heatmap", methods=['POST'])
-# def evaluate_heatmap():
-#     if request.method == 'POST':
-#         file = request.files['file']
-#         annotations = json.load(request.files['data'])['annotations']
-#         image = Image.open(BytesIO(file.read()))
-#         count, heatmap = predict(image, annotations, return_density_map=True)
-#         return {'count': count, 'heatmap': heatmap} 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
