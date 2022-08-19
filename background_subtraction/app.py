b918d5169ee30957417d47f8d34a4c338ab84193
manfredmichael <koskosanrider@gmail.com>
2022-08-19 12:35:43 +0700

    Add encode_image

--- background_subtraction/app.py
+++ background_subtraction/app.py
@@ -1,18 +1,27 @@
 from io import BytesIO
 from xmlrpc.server import resolve_dotted_attribute
 from PIL import Image
-from flask import Flask, request, send_file
+from flask import Flask, request, send_file, jsonify
 from background_subtraction import apply_virtual_background
 from base64 import decodebytes, encodebytes
 import numpy as np
 
 app = Flask(__name__)
 
+def get_response_image(image):
+    return encode_image(Image.fromarray(np.uint8(image)))
+
 def encode_image(pil_img):
     byte_arr = BytesIO()
     pil_img.save(byte_arr, format='PNG') # convert the PIL image to byte array
     encoded_img = encodebytes(byte_arr.getvalue()).decode('ascii') # encode as base64
     return encoded_img
+
+def decode_image(image_bytes):
+    image_bytes = decodebytes(image_bytes)
+    image_bytes = BytesIO(image_bytes)
+    image_bytes = Image.open(image_bytes)
+    return np.array(image_bytes)
 
 def serve_pil_image(pil_img):
     img_io = BytesIO()
@@ -27,13 +36,16 @@ def make_prediction():
         background = request.files['background'].read()
         virtual_background = request.files['virtual_background'].read()
 
-        webcam = np.array(Image.open(BytesIO(decodebytes(webcam))))
-        background = np.array(Image.open(BytesIO(decodebytes(background))))
-        virtual_background = np.array(Image.open(BytesIO(decodebytes(virtual_background))))
+        webcam = decode_image(webcam)
+        background = decode_image(background)
+        virtual_background = decode_image(virtual_background)
 
-        result = apply_virtual_background(webcam, background, virtual_background)
-        result = Image.fromarray(np.uint8(result))
-        return serve_pil_image(result)
+        step_images, result = apply_virtual_background(webcam, background, virtual_background, return_steps=True)
+        result = get_response_image(result)
+        for i in range(len(step_images)):
+            step_images[i][1] = get_response_image(step_images[i][1])
+        return jsonify({'result': result,
+                        'steps': step_images})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
