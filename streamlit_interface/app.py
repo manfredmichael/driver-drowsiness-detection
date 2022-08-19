4a51a1a2b92c97c1d7d180c558cf89da805c10c6
manfredmichael <koskosanrider@gmail.com>
2022-08-19 12:57:55 +0700

    Add demo from live camera page

--- streamlit_interface/app.py
+++ streamlit_interface/app.py
@@ -14,13 +14,7 @@ from PIL import Image
 # from streamlit_drawable_canvas import st_canvas
 from utils import inference
 
-def main():
-    if "button_id" not in st.session_state:
-        st.session_state["button_id"] = ""
-    if "color_to_label" not in st.session_state:
-        st.session_state["color_to_label"] = {}
-
-
+def from_picture():
     st.markdown("### STEP 1: Upload your virtual background image")    
     virtual_background_file = st.file_uploader('Upload virtual background image', type=['jpg', 'jpeg', 'png'], accept_multiple_files=False)
     if virtual_background_file: 
@@ -33,7 +27,7 @@ def main():
         background = Image.open(background_file).convert('RGB')
         st.image(background)
 
-    st.markdown("### STEP 3: Upload YOU!")    
+    st.markdown("### STEP 3: Upload YOU!")   
     webcam_file = st.file_uploader('Upload webcam image', type=['jpg', 'jpeg', 'png'], accept_multiple_files=False)
     if webcam_file: 
         webcam = Image.open(webcam_file).convert('RGB')
@@ -52,6 +46,48 @@ def main():
 
         st.markdown(f"### The result")
         st.image(result)
+
+def from_camera():
+    st.markdown("### STEP 1: Upload your virtual background image")    
+    virtual_background_file = st.file_uploader('Upload virtual background image', type=['jpg', 'jpeg', 'png'], accept_multiple_files=False)
+    if virtual_background_file: 
+        virtual_background = Image.open(virtual_background_file).convert('RGB')
+        st.image(virtual_background)
+
+    st.markdown("### STEP 2: Take your empty background image (tip: hide under the table!)")    
+    background_file = st.camera_input("Take a picture of the background")
+    if background_file: 
+        background = Image.open(background_file).convert('RGB')
+        st.image(background)
+
+    st.markdown("### STEP 3: Take a picture of YOU!")   
+    webcam_file = st.camera_input("Take a picture of yourself") 
+    if webcam_file: 
+        webcam = Image.open(webcam_file).convert('RGB')
+        st.image(webcam)
+
+    if webcam_file and background_file and virtual_background_file:
+        st.markdown("---")
+        st.markdown("# Virtual Background Result")
+
+        steps, result = inference(webcam, background, virtual_background)
+
+        st.markdown(f"### The steps")
+        for step_name, step_image in steps.items():
+            st.markdown(f"##### {step_name}")
+            st.image(step_image)
+
+        st.markdown(f"### The result")
+        st.image(result)
+
+def main():
+    page_names_to_funcs = {
+        "From Uploaded Picture": from_picture,
+        "From Camera": from_camera,
+    }
+
+    selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+    page_names_to_funcs[selected_page]()
 
 if __name__ == "__main__":
     st.set_page_config(
